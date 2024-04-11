cc485a5b7fd5d6a69818c638b4a82bea56fb9d00
P014r-13 <811mohebi@gmail.com>
2024-04-11 23:08:03 +0330

    sign-up and verify url

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import LoginView,LogOutView
+from .views import LoginView,LogOutView,SignUpView,OtpVerifyView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogOutView.as_view(), name='logout')
+    path('logout/', LogOutView.as_view(), name='logout'),
+    path('sign-up/', SignUpView.as_view(), name='signup'),
+    path('verify/', OtpVerifyView.as_view(), name='otp_verify')
 ]
