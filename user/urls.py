e79b8f24708217f21a05e8be7564bf201ea9809e
P014r-13 <811mohebi@gmail.com>
2024-04-11 21:48:01 +0330

    login and logout urls

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import LoginView,LogOutView
 
-
-urlpatterns = []
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogOutView.as_view(), name='logout')
+]
