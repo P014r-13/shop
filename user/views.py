7fdc08fd4def95346ff0af74ab2c58abe6d77a29
P014r-13 <811mohebi@gmail.com>
2024-04-11 21:47:25 +0330

    add Login and LogOutView

--- user/views.py
+++ user/views.py
@@ -1,2 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.contrib.auth import login,authenticate,logout
 
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(email=email,password=password)
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect('/')
+        return render(request, 'login.html')
+
+class LogOutView(View,LoginRequiredMixin):
+    @staticmethod
+    def get(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            request.user.is_logged_in = False
+            request.user.save()
+            logout(request)
+        return HttpResponseRedirect(reverse('login'))
