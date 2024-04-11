4d2b0943893ada0c905f710969b70717fef8efb3
P014r-13 <811mohebi@gmail.com>
2024-04-11 23:08:23 +0330

    add Signup view and otp verify view

--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
+import random
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.views import View
 from django.contrib.auth import login,authenticate,logout
+from .models import User
 
 class LoginView(View):
     def get(self, request):
@@ -17,6 +20,49 @@ class LoginView(View):
             return HttpResponseRedirect('/')
         return render(request, 'login.html')
 
+class SignUpView(View):
+    def get(self, request):
+        return render(request,'sign-up.html')
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if password1 != password:
+            return render(request,'sign-up.html')
+        else:
+            user = User.objects.create_user(email=email,password=password)
+            print(user)
+            login(request,user)
+            user.otp = random.randint(1000,9999)
+            send_mail(
+                'Instagram',
+                f'Your otp code : {user.otp}.',
+                'fortanarmin@gmail.com',
+                [email],
+                fail_silently=False,
+            )
+            user.save()
+            return redirect('otp_verify')
+
+class OtpVerifyView(View):
+    def get(self,request):
+        return render(request,'otp-verify.html')
+    def post(self,request):
+        otp1 = request.POST['otp1']
+        otp2 = request.POST['otp2']
+        otp3 = request.POST['otp3']
+        otp4 = request.POST['otp4']
+        end_otp = f'{otp1}{otp2}{otp3}{otp4}'
+        print(end_otp)
+        print(request.user.otp)
+        if request.user.otp == int(end_otp):
+            print('in ifffffffffffffffffffffffffff')
+            user = request.user
+            user.verify = True
+            user.save()
+            return HttpResponseRedirect('/')
+        else :
+            return render(request,'otp-verify.html')
 class LogOutView(View,LoginRequiredMixin):
     @staticmethod
     def get(request, *args, **kwargs):
