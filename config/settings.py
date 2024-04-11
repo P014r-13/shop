e3c9faa068b11215d8984ab74d50ac24aa15c9bc
P014r-13 <811mohebi@gmail.com>
2024-04-11 23:07:41 +0330

    Email host | email host user | email host password | email port | email use tls | email backend

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,12 @@ AUTH_USER_MODEL = 'user.User'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_REDIRECT_URL = 'login/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'fortanarmin@gmail.com'
+EMAIL_HOST_PASSWORD = 'tfszqaeazescygqo'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
