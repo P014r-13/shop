8409fc30a5b56502fa3cc37582c3ea7494e39070
P014r-13 <811mohebi@gmail.com>
2024-04-11 15:25:32 +0330

    add UserModel

--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
+from .manager import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    otp = models.IntegerField(blank=True,null=True)
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name="custom_user_groups",
+help_text='The groups this user belongs to. A user will get all permissions granted '
+'to each of their groups.')
+
+user_permissions = models.ManyToManyField(Permission, verbose_name='user permissions', blank=True,
+related_name="custom_user_permissions",
+help_text='Specific permissions for this user.')
+
+
