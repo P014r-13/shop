d2af0fcfbfe0a93ce2335c5f3b350570a8a4565a
P014r-13 <811mohebi@gmail.com>
2024-04-19 10:23:06 +0330

    add to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product
-from home.models import Category
+from .models import Product,Category,Address,Order,Order_Item,Code,Comment
+
 
 
 admin.site.register((Product,Category))
+admin.site.register(Address)
+admin.site.register((Order,Order_Item,Code,Comment))
