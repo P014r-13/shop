b5f642c8123a1d238786afc48e62c95565641a95
P014r-13 <811mohebi@gmail.com>
2024-04-19 10:33:24 +0330

    add order url

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView,ProductView,AccountView
+from .views import HomeView,ProductView,AccountView,OrderView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/', ProductView.as_view(), name='products'),
-    path('accounts/', AccountView.as_view(), name='account')
+    path('accounts/', AccountView.as_view(), name='account'),
+    path('order/', OrderView.as_view(), name='Order')
 ]
