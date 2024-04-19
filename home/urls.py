64cdd11dc114dff45893c6516c47d6b92eaf4369
P014r-13 <811mohebi@gmail.com>
2024-04-19 10:27:43 +0330

    accounts/

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView,ProductView
+from .views import HomeView,ProductView,AccountView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('products/', ProductView.as_view(), name='products')
+    path('products/', ProductView.as_view(), name='products'),
+    path('accounts/', AccountView.as_view(), name='account')
 ]
